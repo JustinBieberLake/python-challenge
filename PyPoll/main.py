a1b58189e5eef458a6e0a521b8501942d499c54a
Eric Martinez <ericandrewmartinez@gmail.com>
2022-11-05 19:30:19 -0500

    fixed path to export txt file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -39,9 +39,35 @@ with open(csvpath) as csvfile:
 
 print('\n')
 print("Election Results")
-print("-"*40)
+print("-"*60)
 
 # Total Votes
 print(f"Total Votes: {len(ballotID)}")
-print("-"*40)
-# 
+print("-"*60)
+
+# Percentage of Votes Based on Total Votes and Candidate Votes
+print(f"Charles Casper Stockham: {round(((Charlie/len(ballotID))*100),3)}% ({Charlie}")
+print(f"Diana DeGette: {round(((Diana/len(ballotID))*100),3)}% ({Diana}")
+print(f"Raymon Anthony Doane: {round(((Raymon/len(ballotID))*100),3)}% ({Raymon}")
+print("-"*60)
+
+# And the winner is
+print(f"Winner: {winner}")
+print("-"*60)
+print('\n')
+
+# Export Text File
+outputpath=os.path.join("PyPoll","Analysis","poll_analysis.txt")
+
+with open(outputpath, 'w') as csvfile:
+    csvwriter=csv.writer(csvfile)
+    csvwriter.writerow(["Election Results"])
+    csvwriter.writerow(["-"*60])
+    csvwriter.writerow(f"Total Votes: {len(ballotID)}")
+    csvwriter.writerow(["-"*60])
+    csvwriter.writerow(f"Charles Casper Stockham: {round(((Charlie/len(ballotID))*100),3)}% ({Charlie}")
+    csvwriter.writerow(f"Diana DeGette: {round(((Diana/len(ballotID))*100),3)}% ({Diana}")
+    csvwriter.writerow(f"Raymon Anthony Doane: {round(((Raymon/len(ballotID))*100),3)}% ({Raymon}")
+    csvwriter.writerow(["-"*60])
+    csvwriter.writerow(f"Winner: {winner}")
+    csvwriter.writerow("-"*60)
